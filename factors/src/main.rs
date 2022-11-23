cc404ff5377869b39bfe58ec5135f61fc18c56b6
Qredo LTD <support@qredo.com>
2022-11-23 20:07:07 +0800

    Following instructions

--- factors/src/main.rs
+++ factors/src/main.rs
@@ -1,13 +1,13 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method is `multiply`, replace `METHOD_NAME_ID` with `MULTIPLY_ID` and replace `METHOD_NAME_PATH` with `MULTIPLY_PATH`
-use methods::{METHOD_NAME_ID, METHOD_NAME_PATH};
+use methods::{MULTIPLY_ID, MULTIPLY_PATH};
 use risc0_zkvm::host::Prover;
 // use risc0_zkvm::serde::{from_slice, to_vec};
 
 fn main() {
     // Make the prover.
-    let method_code = std::fs::read(METHOD_NAME_PATH)
+    let method_code = std::fs::read(MULTIPLY_PATH)
         .expect("Method code should be present at the specified path; did you use the correct *_PATH constant?");
-    let mut prover = Prover::new(&method_code, METHOD_NAME_ID)
+    let mut prover = Prover::new(&method_code, MULTIPLY_ID)
         .expect("Prover should be constructed from valid method source code and corresponding method ID");
 
     // TODO: Implement communication with the guest here
@@ -17,7 +17,7 @@ fn main() {
         .expect("Valid code should be provable if it doesn't overflow the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
 
     // Optional: Verify receipt to confirm that recipients will also be able to verify your receipt
-    receipt.verify(METHOD_NAME_ID)
+    receipt.verify(MULTIPLY_ID)
         .expect("Code you have proven should successfully verify; did you specify the correct method ID?");
 
     // TODO: Implement code for transmitting or serializing the receipt for other parties to verify here
