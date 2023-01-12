d2661e799b9235f19c844df1e64b19913ab28d32
Qredo LTD <support@qredo.com>
2023-01-12 03:26:35 +0000

    Turn off C compiling for now

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,7 +1,7 @@
 #![no_main]
 #![no_std]  // std support is experimental, but you can remove this to try it
 
-extern { fn fib(x: i32) -> i32; }
+// extern { fn fib(x: i32) -> i32; }
 
 use risc0_zkvm::guest::{env};
 use risc0_zkvm_platform::io::SENDRECV_CHANNEL_INITIAL_INPUT;
@@ -11,8 +11,8 @@ risc0_zkvm::guest::entry!(main);
 pub fn main() {
     let initial_input: &'static [u8] = env::send_recv(SENDRECV_CHANNEL_INITIAL_INPUT, &[]);
     // let initial_input: &[u8] = env::read();
-    let answer: u32 = initial_input.iter().map(|&x| x as u32).sum();
-    // let answer = initial_input.len();
-    let fanswer: i32 = unsafe { fib(10) };
-    env::commit(&fanswer);
+    // let answer: u32 = initial_input.iter().map(|&x| x as u32).sum();
+    let answer = initial_input.len();
+    // let fanswer: i32 = unsafe { fib(10) };
+    env::commit(&answer);
 }
